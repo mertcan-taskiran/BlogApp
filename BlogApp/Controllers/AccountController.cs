4ecd00e89537eda17b507c9c6393b9a518a29381
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-04-03 17:09:08 +0300

    Register

---
+++ BlogApp/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using BlogApp.Identity;
+using BlogApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> UserManager;
+        private RoleManager<ApplicationRole> RoleManager;
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            UserManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<ApplicationRole>(new IdentityDataContext());
+            RoleManager = new RoleManager<ApplicationRole>(roleStore);
+        }
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.Name = model.Name;
+                user.Surname = model.Surname;
+                user.UserName = model.Username;
+                user.Email = model.Email;
+
+                var result = UserManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    if (RoleManager.RoleExists("user"))
+                    {
+                        UserManager.AddToRole(user.Id,"user");
+                    }
+
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("Hata", "Kullanıcı Oluşturma Hatası");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ BlogApp/Models/Register.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogApp.Models
+{
+    public class Register
+    {
+        [Required]
+        //[DisplayName("Adınız")]
+        public string Name { get; set; }
+        [Required]
+        //[DisplayName("Soyadınız")]
+        public string Surname { get; set; }
+        [Required]
+        //[DisplayName("Kullanıcı Adı")]
+        public string Username { get; set; }
+        [Required]
+        //[DisplayName("Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        //[DisplayName("Şifre")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        //[DisplayName("Şifre Tekrar")]
+        [Compare("Password", ErrorMessage = "Password does not match")]
+        public string RePassword { get; set; }
+
+    }
+}
