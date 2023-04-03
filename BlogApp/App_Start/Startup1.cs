ad264401bfed31094db1629fb09b1e2803a46206
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-04-03 15:20:42 +0300

    Identity için bileşenlerin yüklenmesi

---
+++ BlogApp/App_Start/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(BlogApp.Startup1))]
+
+namespace BlogApp
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions()
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Account/Login")
+            });
+        }
+    }
+}
