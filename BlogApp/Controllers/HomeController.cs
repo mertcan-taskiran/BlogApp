fd59d184abbb5e2369067087ac54201d9a0738c6
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-04-03 14:52:49 +0300

    Detay sayfaları aktif edildi

--- BlogApp/Controllers/HomeController.cs
+++ BlogApp/Controllers/HomeController.cs
@@ -13,12 +13,29 @@ namespace BlogApp.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var blog = db.Bloglar.Where(i => i.Onay == true && i.Anasayfa == true).OrderByDescending(i => i.EklenmeTarihi);
+            var blog = db.Bloglar
+                .Where(i => i.Onay == true && i.Anasayfa == true)
+                .Select(i=>new BlogModel(){
+                    Id = i.Id,
+                    Baslik = i.Baslik,
+                    KullaniciAdi = i.KullaniciAdi,
+                    Resim = i.Resim,
+                    EklenmeTarihi = i.EklenmeTarihi,
+                    Onay = i.Onay,
+                    Anasayfa = i.Anasayfa,
+                    Goruntulenme = i.Goruntulenme,
+                    Yorum = i.Yorum,
+                    Aciklama = i.Aciklama.Length>50?i.Aciklama.Substring(0,50)+("..."):i.Aciklama,
+                    Icerik = i.Icerik     
+                })
+                .OrderByDescending(i => i.EklenmeTarihi).ToList();
 
             return View(blog);
         }
-        public ActionResult Detay()
+        public ActionResult Detay(int id)
         {
+            var blog = db.Bloglar.Find(id);
+            ViewBag.blog = blog;
             return View();
         }
 
---
+++ BlogApp/Models/BlogModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogApp.Models
+{
+    public class BlogModel
+    {
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        public string Resim { get; set; }
+        public string Icerik { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public int Goruntulenme { get; set; }
+        public bool Onay { get; set; }
+        public bool Anasayfa { get; set; }
+        public string KategoriAdi { get; set; }
+        public virtual ICollection<Yorum> Yorum { get; set; }
+    }
+}
