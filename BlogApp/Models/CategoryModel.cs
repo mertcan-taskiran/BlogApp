85a1c61c3b69836e0b62eef75c340eaf098c32c3
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-04-08 03:25:06 +0300

    Blog Sayısı Öğrenme

--- BlogApp/Models/CategoryModel.cs
+++ BlogApp/Models/CategoryModel.cs
@@ -5,11 +5,10 @@ using System.Web;
 
 namespace BlogApp.Models
 {
-    public class Kategori
+    public class CategoryModel
     {
         public int Id { get; set; }
         public string KategoriAdi { get; set; }
-
-        public List<Blog> Bloglar { get; set; }
+        public int BlogSayisi { get; set; }
     }
 }
